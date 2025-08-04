0545cf7332885de0ff25500bcc0d0ea2ad341d5e
sridharan-prof <sridharan.prof@gmail.com>
2025-08-04 17:06:49 +0530

    modified email templates

--- addons/employee_self_service/models/employee_feedback.py
+++ addons/employee_self_service/models/employee_feedback.py
@@ -5,7 +5,11 @@ class employee_feedback(models.Model):
     _description = "employee feedback"
 
     name = fields.Char(string='Title')
-    feedback_type = fields.Selection([('hr', 'HR'), ('tech', 'Tech'), ('general', 'General')])
+    feedback_type = fields.Selection([('hr', 'HR'), ('tech', 'Tech'), ('general', 'General')], default = "general")
     message = fields.Text(string='Message', required=True)
     is_anonymous = fields.Boolean(string='Submit Anonymously', default=False)
     employee_id = fields.Many2one('hr.employee', string='Submitted By')
+
+    def hr_action(self):
+        for rec in self:
+            rec.status = 'hr_review'
